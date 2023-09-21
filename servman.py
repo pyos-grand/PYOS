dd8996934f0112f62b1534d47dd94cd51036f38c
mivinac <root@goorm.io>
2023-09-21 11:37:15 +0000

    Updated service: configs

--- servman.py
+++ servman.py
@@ -6,3 +6,9 @@ def update_services(servname):
 		av = bb.read()
 		print("Running service, please wait...")
 		exec(av)
+
+def update_conf(servname):
+	print("PyOS Servman Utility 0.0.5")
+	with open('conf/'+servname+'service.conf') as bb:
+		av = bb.read()
+		print(av)
