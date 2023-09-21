5f0689524ff9d386f674c7e5cd1694509db82737
mivinac <root@goorm.io>
2023-09-21 15:53:12 +0000

    Testing feature with editing config...

--- servman.py
+++ servman.py
@@ -12,3 +12,10 @@ def update_conf(servname):
 	with open('conf/'+servname+'service.conf') as bb:
 		av = bb.read()
 		print(av)
+
+
+
+def edit_conf(servname):
+	print("PyOS Servman Utility 0.0.5")
+	with open('conf/'+servname+'service.conf') as bf:
+		print("Please wait, this feature isn't working right now.")
