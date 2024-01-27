a3672337c06e103a9abd1eccf463f2ac3cd71885
RomanZSeldinov <roman5601roma@hotmail.com>
2024-01-27 15:05:28 +0300

    Add main code in powerman.py

--- powerman.py
+++ powerman.py
@@ -2,6 +2,49 @@ import os
 import sys
 import time
 import lib.filestream as fs
+import interactiveshell as shell
+import fmtest
+
+def maintools():
+    ascii = """
+    
+    """
+    print(ascii)
+    modes = """
+    1. Text editor
+    2. Shell
+    3. Other tools
+    4. Exit powerman
+    """
+    while (True):
+        modechoose = input("Tool: ")
+        if (modechoose == "1"):
+            text_editor()
+        elif (modechoose == "2"):
+            interactiveshell.console()
+        elif (modechoose == "3"):
+            pass
+        elif (modechoose == "4"):
+            exit()
+
+def text_editor():
+    fmtest.function_select()
+
+def powerselect():
+    print("Select Mode")
+    modes = """
+    1. Text editor, shell and tools
+    2. Text editor
+    """
+    modechoose = input("Mode: ")
+    if (modechoose == "1"):
+        maintools()
+    elif (modechoose == "2"):
+        pass
+    elif (modechoose == "3"):
+        pass
+    else:
+        pass
 
 def powerstart():
     ascii = """
@@ -17,7 +60,7 @@ def powerstart():
     print(ascii)
     print("POWERMAN v. 0.0.1")
     print("(c) 2024 PyOS developers")
-
+    
 
 
     
